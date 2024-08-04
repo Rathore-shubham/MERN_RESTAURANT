2e253d09099368bd86fdaf66870e81f165f29993
Shubham Rathore <sr1849587@gmail.com>
2024-08-04 17:53:44 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Restuarent</div>
+          <div className="right">
+            <p>17 bhawarkua, Indore</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
